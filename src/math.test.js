f89c9c8fad5b74293dcb029ebfd4f8a48fd19fdf
Milos Davidovic <milos.davidovic@embracesbs.com>
2020-05-24 17:48:55 +0200

    added expect function

--- src/math.test.js
+++ src/math.test.js
@@ -1,19 +1,28 @@
-function multiply(a, b) {
-  return a * b;
+const { divide, multiply } = require("./math");
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${expected} is not equal to ${actual}`);
+      }
+    },
+    toEqual(value) {
+      throw new Error("Not implemented exception");
+    },
+    toBeGreaterThen(value) {
+      throw new Error("Not implemented exception");
+    },
+  };
 }
 
-function divide(a, b) {
-  return a / b;
-}
+let expected;
+let result;
 
-let expected = 16;
-let result = multiply(4, 4);
-if (expected !== result) {
-  throw new Error(`${expected} is not equal to ${result}`);
-}
+expected = 15;
+result = multiply(4, 4);
+expect(result).toEqual(expected);
 
 expected = 2;
 result = divide(4, 2);
-if (expected !== result) {
-  throw new Error(`${expected} is not equal to ${result}`);
-}
+expect(result).toEqual(expected);
